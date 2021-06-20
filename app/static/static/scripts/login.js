7815e776d685c18e8e5e854ba87ad5ffc2b08431
Joe O'Connor <joeoc2625@gmail.com>
2021-06-20 21:18:53 +0100

    Fix login

--- app/static/static/scripts/login.js
+++ app/static/static/scripts/login.js
@@ -1,39 +1,36 @@
-class Login {
-    static googleInit() {
+const login = (function () {
+    let obj = {};
+
+    obj.googleInit = function () {
         gapi.load('auth2', function () {
             // Done
         });
     }
 
-    static onGoogleSignIn(googleUser) {
-        const id_token = googleUser.getAuthResponse().id_token;
-
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '/api/login_google');
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = function () {
-            if (xhr.status !== 200) {
-                Login.onLoginFail(xhr.responseText);
-                return;
-            }
-            const error_message = $("#login-error-msg");
-            error_message.hide();
-            console.log('Signed in as: ' + xhr.responseText);
-            window.location.replace("/leaderboard");
-        };
-        xhr.onerror = function () {
-            Login.onLoginFail(xhr.responseText);
-        };
-        xhr.send(JSON.stringify({
-            idtoken: id_token
-        }));
-    }
-
-    static onLoginFail(error) {
+    obj.onLoginFail = function (error) {
         console.log("Could not sign in");
         console.log(error);
 
         const error_message = $("#login-error-msg");
         error_message.show();
     }
-}
+
+    obj.onGoogleSignIn = function (googleUser) {
+        const id_token = googleUser.getAuthResponse().id_token;
+
+        $.ajax({
+            url: '/api/exchange_google_token',
+            type: 'POST',
+            data: JSON.stringify({google_token: id_token}),
+            contentType: 'application/json; charset=utf-8'
+        })
+            .then(response => {
+                const error_message = $("#login-error-msg");
+                error_message.hide();
+                console.log(response);
+            })
+            .catch(response => obj.onLoginFail(response));
+    }
+
+    return obj;
+}());
