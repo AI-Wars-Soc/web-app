eab8d3ca72604634576f774ef3937115c04f8fef
Joe O'Connor <joeoc2625@gmail.com>
2021-07-03 01:39:26 +0100

    Switch navbar to react-bootstrap

--- app/src/navbar.tsx
+++ app/src/navbar.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-import {
-    NavLink
-} from "react-router-dom";
-
-import { Navbar } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 
 
 type NavItemProps = {
     active: boolean,
-    data_toggle: string,
+    data_toggle: string | undefined,
     link: string,
     text: string,
     icon: string
@@ -19,10 +15,8 @@ class NavItem extends React.Component<NavItemProps> {
     render(): JSX.Element {
         const {link, text, icon, data_toggle} = this.props;
         const active = (link === window.location.pathname);
-        return <li className={ "nav-item" + (active ? " active" : "")}>
-            <a className="nav-link" href={link} data-toggle={data_toggle}>{text}&nbsp;
-                {icon !== null && <i className={icon}/>}</a>
-        </li>;
+        return <Nav.Link href={link} data-toggle={data_toggle} active={active}>{text}&nbsp;
+                {icon !== null && <i className={"bi bi-" + icon}/>}</Nav.Link>;
     }
 }
 
@@ -51,7 +45,7 @@ export class MyNavbar extends React.Component<Record<string, never>, NavbarState
         const requestOptions = {
             method: 'POST'
         };
-        fetch("http://lvh.me/api/get_navbar", requestOptions)
+        fetch("/api/get_navbar", requestOptions)
             .then(res => res.json())
             .then(
                 (result) => {
@@ -79,24 +73,22 @@ export class MyNavbar extends React.Component<Record<string, never>, NavbarState
 
         let error_div = <></>;
         if (error) {
-            error_div = <i className="bi bi-cloud-slash"/>;
+            error_div = <i className="bi bi-cloud-slash" role="img" aria-label="Network Error"/>;
         }
 
         return <Navbar bg="dark" variant="dark" expand="md">
-            <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-                <ul className="navbar-nav mr-auto">
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
                     {l_nav}
-                </ul>
-            </div>
+                </Nav>
+            </Navbar.Collapse>
             <Navbar.Brand href="/" className="mx-auto">{ navbar.soc_name } { error_div }</Navbar.Brand>
-            <div className="collapse navbar-collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
                     {r_nav}
-                </ul>
-            </div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-                <span className="navbar-toggler-icon"/>
-            </button>
+                </Nav>
+            </Navbar.Collapse>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         </Navbar>;
     }
 }
