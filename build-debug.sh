7cb2dcf5a2c32235318b8a40bfb8343f0c8f6cc9
Joe O'Connor <joeoc2625@gmail.com>
2021-08-01 21:46:08 +0100

    Fix up some formatting

--- build-debug.sh
+++ build-debug.sh
@@ -2,10 +2,18 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+mode='dev'
+while getopts 'p' flag; do
+    case "${flag}" in
+        p) mode='prod';;
+		*) printf '\nUsage: %s: [-p]roduction\n' "$0"; exit 2 ;;
+    esac
+done
+
 mkdir /tmp/web-app_build/
 rsync -av --progress "$SCRIPT_DIR"/app/ /tmp/web-app_build/ --exclude dist --exclude node_modules 
 
-(cd /tmp/web-app_build/ || exit; npm i; npm run dev)
+(cd /tmp/web-app_build/ || exit; npm i; npm run "$mode")
 
 rm "$SCRIPT_DIR"/app/dist/*
 rsync -av --progress /tmp/web-app_build/dist/ "$SCRIPT_DIR"/app/dist/
