040b5bc09f160f2d197bb5d616de6fc895178635
Joe O'Connor <joeoc2625@gmail.com>
2021-08-03 18:18:35 +0100

    Added a Service Worker

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -16,6 +16,15 @@ const GamesPage = React.lazy(() => import("./games/gamesPage"));
 const MePage = React.lazy(() => import("./me/mePage"));
 const MyFooter = React.lazy(() => import("./footer"));
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
 
 function App(): JSX.Element {
     const [user, setUser] = useState(null as UserData);
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,6 +5,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const transformer = require('ts-type-checked/transformer');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
@@ -12,13 +13,18 @@ const htmlPlugin = new HtmlWebPackPlugin({
 });
 
 const tsTypeCheckerPlugin = new ForkTsCheckerWebpackPlugin({
-    async: false,
+    async: true,
     eslint: {
         files: ["./src/**.tsx"],
     },
 });
 
 const miniCss = new MiniCssExtractPlugin();
+
+const workerBox = new WorkboxPlugin.GenerateSW({
+    clientsClaim: true,
+    skipWaiting: true,
+});
 
 module.exports  = {
     entry: "./src/index.tsx",
@@ -31,7 +37,7 @@ module.exports  = {
     resolve: {
         extensions: ['.tsx', '.ts', '.jsx', '.js'],
     },
-    plugins: [htmlPlugin, tsTypeCheckerPlugin, miniCss],
+    plugins: [htmlPlugin, workerBox, tsTypeCheckerPlugin, miniCss],
     module: {
         rules: [
             {
