1b38965afd723157dac202e83f790891a92ae6d4
Joe O'Connor <joeoc2625@gmail.com>
2021-09-30 17:59:41 +0100

    Update Me page to use better react

--- app/src/me/mePage.tsx
+++ app/src/me/mePage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {User} from "../user";
 import RealNameSwitch from "./realNameSwitch";
 import {DeleteAccountButton} from "./deleteAccountButton";
+import {PageInfo, PageTitle} from "../pageTitleAndInfo";
+import {Container, Row} from "react-bootstrap";
 
 type MePageProps = {
     user: User,
@@ -20,18 +22,11 @@ export default class MePage extends React.Component<MePageProps, MePageState> {
 
     private static getPage(realName = "", nickname = "", controls: JSX.Element[] = []): JSX.Element {
         return <>
-            <div className="flex-column justify-content-center justify-content-sm-start">
-                <h1>{realName}</h1>
-                <h3 className="nickname-text">({nickname})</h3>
-            </div>
-            <div className="px-3 d-none d-sm-block">
-                <p className="lead">
-                    This is your settings page
-                </p>
-            </div>
-            <div className="flex-column justify-content-center justify-content-sm-start px-3 m-lg-3">
-                {controls.map((c, i) => <div className="flex-row" key={"userControl" + i}>{c}</div>)}
-            </div>
+            <PageTitle>{realName}</PageTitle>
+            <PageInfo>({nickname})</PageInfo>
+            <Container className="px-3 m-lg-3">
+                {controls.map((c, i) => <Row className={""} key={"userControl" + i}>{c}</Row>)}
+            </Container>
         </>;
     }
 
--- app/src/pageTitleAndInfo.tsx
+++ app/src/pageTitleAndInfo.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export function PageTitle(props: {children: string}): JSX.Element {
+export function PageTitle(props: {children: React.ReactNode}): JSX.Element {
     return <div className="flex-column justify-content-center justify-content-sm-start">
             <h1>{props.children}</h1>
         </div>;
 }
 
-export function PageInfo(props: {children: string}): JSX.Element {
+export function PageInfo(props: {children: React.ReactNode}): JSX.Element {
     return <div className="px-3 d-none d-sm-block">
         <p className="lead">
             {props.children}
